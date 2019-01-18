6fd83bde559e6941f03a9bbe6e3c1659ff1a12c6
blabla1337 <glenntencate@gmail.com>
2019-01-18 16:24:51 +0100

    Add CMD lab + fix old

---
+++ CMD/CMD.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask, request, url_for, render_template, redirect
+
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.config['DEBUG'] = True
+
+@app.route("/")
+def start():
+    return render_template("index.html")
+
+
+@app.route("/home", methods=['POST'])
+def home():
+    sizeImg = request.form['size']
+    os.system('convert static/img/bones.png -resize '+sizeImg+'% ')
+    return render_template("index.html")
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
+	
+
+
