c7170455cac462eb07ad2a2b5f9cd4c6a0ae036e
blabla1337 <glenntencate@gmail.com>
2019-01-20 15:53:50 +0100

    fix db

--- Auth-bypass-simple/config/initializer.py
+++ Auth-bypass-simple/config/initializer.py
@@ -14,10 +14,10 @@ with con:
     cur.execute("CREATE TABLE users(UserId INT, UserName TEXT, Password TEXT)")
     cur.execute("INSERT INTO users VALUES(1,'admin','admin')")
 
-    
     #Create some data for pageinformation
-    cur.execute("CREATE TABLE prefs(PreferenceId INT, Color TEXT, UserId)")
-    cur.execute("INSERT INTO prefs VALUES(1,'RED', 1)")
-    
+    cur.execute("CREATE TABLE prefs_users(PreferenceId INT, API_key TEXT, UserId)")
+    cur.execute("INSERT INTO prefs_users VALUES(1,'5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8', 1)")
+    cur.execute("INSERT INTO prefs_users VALUES(1,'cbfdac6008f9cab4083784cbd1874f76618d2a97', 1)")
+
     con.commit()
     #con.close()
