c53c9ca9e9884b8b847b939383477460b6f2a6d8
giuliocomi <giulio1.comi@mail.polimi.it>
2019-02-19 02:09:17 +0100

    ssrf example in flask

--- SSRF/SSRF.py
+++ SSRF/SSRF.py
@@ -1,27 +1,35 @@
-from flask import Flask, request, url_for, render_template, redirect
+from flask
+import Flask, request, url_for, render_template, redirect
 
-app = Flask(__name__, static_url_path='/static', static_folder='static')
+app = Flask(__name__, static_url_path = '/static', static_folder = 'static')
+
 app.config['DEBUG'] = True
 
-@app.route("/", methods=['GET'])
+@app.route("/", methods = ['GET'])
+
 def ssrf():
-    url = request.form['url']
-    try:
-    r = requests.get(url)
-    except requests.exceptions.RequestException as e:
-	if "refused" in str(e):
-	    result = "Target not reacheable."
-        else
-	    result = "Target resource is reacheable!"
-	
-    result = "Webpage found."
 
-    return render_template("index.html", result = result)
+  url = request.form['url']
 
+try:
 
+r = requests.get(url)
+
+except requests.exceptions.RequestException as e:
+
+  if "refused" in str(e):
+
+  result = "Target not reacheable."
+
+else
+
+  result = "Target resource is reacheable!"
+
+result = "Webpage found."
+
+return render_template("index.html", result = result)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
-	
 
+  app.run(host = '0.0.0.0')
 
