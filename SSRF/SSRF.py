b80ba2bee77e18807af6a4a955c03d2860bd6514
giuliocomi <giulio1.comi@mail.polimi.it>
2019-02-20 01:45:49 +0100

    SSRF challenge implemented

--- SSRF/SSRF.py
+++ SSRF/SSRF.py
@@ -1,22 +1,28 @@
 from flask import Flask, request, url_for, render_template, redirect
+import requests, validators
 
 app = Flask(__name__, static_url_path = '/static', static_folder = 'static')
 app.config['DEBUG'] = True
 
-@app.route("/", methods = ['GET'])
+@app.route("/")
+def start():
+    return render_template("index.html")
+
+@app.route("/check_existence", methods = ['POST'])
 def ssrf():
     url = request.form['url']
-    try:
-        print("foobar")
-        r = requests.get(url)
-    except requests.exceptions.RequestException as e:
-        if "refused" in str(e):
-             result = "Target not reacheable."
-        else:
-            result = "Target resource is reacheable!"
-    result = "Webpage found."
-    return render_template("index.html", result = result)
 
+    if not validators.url(url):
+        return render_template("index.html", result = "Target resource is not reacheable.") 
+
+    try:
+        requests.head(url, timeout=2.000)
+        return render_template("index.html", result = "Webpage found!")
+    except Exception as e:
+        if "NewConnectionError" in str(e):
+            return render_template("index.html", result = "Target resource is not reacheable.") 
+        else:
+            return render_template("index.html", result = "Target resource is reacheable!")
+    
 if __name__ == "__main__":
   app.run(host = '0.0.0.0')
-
