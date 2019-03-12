c9f653efa0b57b883403ef02b64017f26b9152cb
giuliocomi <giulio1.comi@mail.polimi.it>
2019-03-12 19:15:23 +0100

    DNS Rebinding challenge

--- DNS-rebinding/rebind.py
+++ DNS-rebinding/rebind.py
@@ -31,7 +31,7 @@ def rebind():
     return render_template("index.html", read = "The service to access is listening on localhost (port 80). For DNS rebinding attacks you are expected to control the DNS resolution. </br>For such purpose, you have to setup an authoritative DNS for one of your domains... or use one already <a href=\"https://github.com/brannondorsey/whonow\">available</a>.")
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0:5000')
 
 	
 
