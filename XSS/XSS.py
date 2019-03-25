f53033b1b6a25fe01121bdd29a860d0935355c46
blabla1337 <glenntencate@gmail.com>
2019-03-25 15:59:46 +0100

    test

--- XSS/XSS.py
+++ XSS/XSS.py
@@ -1,5 +1,5 @@
+#!/usr/local/bin/python3
 from flask import Flask, request, url_for, render_template, redirect
-
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config['DEBUG'] = True
