42e5b471db03c528870372404082fb1180e9d007
blabla1337 <glenntencate@gmail.com>
2019-04-12 14:12:52 +0200

    clean dirty

--- docker.sh
+++ docker.sh
@@ -3,7 +3,6 @@
 #Dirrrty, the way how i like it ^^
 for i in $(ls -d */); 
     do 
-path=$(pwd)
     dir=${i%%/};
     echo "cd" $dir/Docker"; docker build . -t" $dir "--no-cache; docker push blabla1337/owasp-skf-lab:"$dir"; cd ../.." | tr "[:upper:]" "[:lower:]" 
 done
