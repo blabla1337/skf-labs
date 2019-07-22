cedc531d33438b11aae22ec087a64e38dbea2681
shayu <shayubit@gmail.com>
2019-07-22 12:18:08 -0400

    New lab added: http-response-splitting

---
+++ http-response-splitting/http-response-splitting.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, url_for, render_template, redirect, Markup
+import flask
+import os
+import subprocess
+import time
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.config['DEBUG'] = True
+
+
+@app.route("/", methods=['GET'])
+def start():
+    return render_template("index2.html")
+
+
+@app.route("/web", methods=['GET'])
+def home():
+    sleepingtime = request.args.get('numero')
+    print(sleepingtime)
+
+    return render_template("index.html", read = flask.Markup(sleepingtime))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
