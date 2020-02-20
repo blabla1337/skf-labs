0b6648128d434126372323f77a618980b99e2d32
mzfr <Mehtab.zafar98@gmail.com>
2020-02-20 19:27:21 +0530

    Handle exceptions and remove unused imports

--- XXE/XXE.py
+++ XXE/XXE.py
@@ -1,8 +1,6 @@
-from flask import Flask, request, url_for, render_template, redirect
-from xml.dom import minidom
-
 from xml.dom.pulldom import START_ELEMENT, parseString
-
+import xml
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config['DEBUG'] = True
@@ -16,11 +14,15 @@ def start():
 @app.route("/home", methods=['POST', 'GET'])
 def xxe():
     doc = parseString(request.form['xxe'])
-    for event, node in doc:
-        if event == START_ELEMENT and node.localName == "items":
-            doc.expandNode(node)
-            nodes = node.toxml()
-    return render_template("index.html", nodes = nodes)
+    try:
+        for event, node in doc:
+            if event == START_ELEMENT and node.localName == "items":
+                doc.expandNode(node)
+                nodes = node.toxml()
+        return render_template("index.html", nodes=nodes)
+    except (UnboundLocalError, xml.sax._exceptions.SAXParseException):
+        return render_template("index.html")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
