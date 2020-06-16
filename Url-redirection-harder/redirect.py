d71051421d0b2735c6d2f946970694cc1f935ed4
Glenn ten Cate <glenntencate@gmail.com>
2020-06-16 09:27:54 +0200

    Update redirect.py

--- Url-redirection-harder/redirect.py
+++ Url-redirection-harder/redirect.py
@@ -4,15 +4,11 @@ app = Flask(__name__, static_url_path='/static', static_folder='static')
 app.config['DEBUG'] = True
 
 
-
 def blacklist(url):
-
 	blacklist = ["."]
-
 	for b in blacklist:
 		if url.find(b) != -1:
 			return True
-
 	return False
 
 
@@ -20,19 +16,19 @@ def blacklist(url):
 def start():
     return render_template("index.html", content = "Sorry, this is the old website.")
 
+
 @app.route("/redirect", methods=['POST', 'GET'])
 def redirector():
     landing_page = request.args.get('newurl')
-
     if blacklist(landing_page):
     	return render_template("index.html", content = "Sorry, you cannot use \".\" in the redirect")
-
-
     return redirect(landing_page, 302)
+
 
 @app.route("/newsite")
 def newsite():
     return render_template("index.html", content = "Welcome to the new website!")
+
 
 @app.errorhandler(404)
 def page_not_found(e):
