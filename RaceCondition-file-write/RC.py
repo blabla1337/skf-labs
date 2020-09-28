4a1b996e9ed80ad5b9d47016a8ec6dc0a39f1589
Riiecco <r.tencate77@gmail.com>
2020-09-28 09:32:28 +0200

    new RaceCondition lab

---
+++ RaceCondition-file-write/RC.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, url_for, render_template, redirect, send_file, make_response
+import os
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+app.config['DEBUG'] = True
+
+
+@app.route("/")
+def start():
+    return render_template("index.html")
+
+@app.route("/<string:value>", methods=['GET'])
+def home(value):
+    # Create a Python file object using open() and the with statement
+    with open("shared-file.txt", 'w') as f:
+        f.write(value)
+        f.closed
+        f.closed
+    file = open("shared-file.txt", "r") 
+    response = make_response(send_file("shared-file.txt", attachment_filename="shared-file.txt"))
+    response.headers.set("Content-Type", "text/html; charset=utf-8")
+    response.headers.set("Content-Disposition", "attachment; filename=shared-file.txt")
+    return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0')
