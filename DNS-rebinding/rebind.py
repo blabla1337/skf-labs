fd4adac37d685becc388bb280ccac1e7d5be0f9e
Michael.Schouwenaar <michael.schouwenaar@ing.com>
2021-12-13 03:44:16 -0500

    Fixed wrong indentation

--- DNS-rebinding/rebind.py
+++ DNS-rebinding/rebind.py
@@ -26,7 +26,7 @@ def rebind():
             else:
                 return render_template("index.html", read = "Only loopback requests are allowed here.")
         except:
-                return render_template("index.html", read = "The service to access is listening on localhost (port 80). For DNS rebinding attacks you are expected to control the DNS resolution. </br>For such purpose, you have to setup an authoritative DNS for one of your domains... or use one already <a href=\"https://github.com/brannondorsey/whonow\">available</a>.")
+            return render_template("index.html", read = "The service to access is listening on localhost (port 80). For DNS rebinding attacks you are expected to control the DNS resolution. </br>For such purpose, you have to setup an authoritative DNS for one of your domains... or use one already <a href=\"https://github.com/brannondorsey/whonow\">available</a>.")
 
     return render_template("index.html", read = "The service to access is listening on localhost (port 80). For DNS rebinding attacks you are expected to control the DNS resolution. </br>For such purpose, you have to setup an authoritative DNS for one of your domains... or use one already <a href=\"https://github.com/brannondorsey/whonow\">available</a>.")
 
