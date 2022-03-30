89aa152c9597c7e8e546aaf925d956f652cedc10
tdimbs <46705963+tdimbs@users.noreply.github.com>
2022-03-30 15:47:53 +0200

    Java labs Dockerfile and pom.xml fixes (#148)
    Co-authored-by: Thiago Dimbarre <thiago.dimbarre@ing.com>

--- java/ldap-injection/src/main/java/com/skf/labs/ldapinjection/LdapInjectionApplication.java
+++ java/ldap-injection/src/main/java/com/skf/labs/ldapinjection/LdapInjectionApplication.java
@@ -1,13 +1,26 @@
 package com.skf.labs.ldapinjection;
 
+import java.io.IOException;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
 public class LdapInjectionApplication {
 
-	public static void main(String[] args)  {
+	public static void main(String[] args) throws IOException, InterruptedException  {
 		SpringApplication.run(LdapInjectionApplication.class, args);
 	}
 
+	@Bean
+    ApplicationRunner applicationRunner(Environment environment) {
+        return args -> {
+            LdapInjectionModel.init(environment.getProperty("init.command"));
+        };
+    }
 }
