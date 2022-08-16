a64de5ee81b59e9f227330ea68a362151d4b6138
Riccardo ten Cate <riccardo.tencate@nvc3406.nedap.local>
2022-08-16 16:33:32 +0200

    threatmodeling lab

---
+++ python/Threat-modeling/config/initializer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+
+con = lite.connect('../Database.db')
+
+with con:
+    
+    cur = con.cursor()
+    
+    #Create data for the user table
+    cur.execute("CREATE TABLE users(UserId INT, UserName TEXT, Password TEXT)")
+    cur.execute("INSERT INTO users VALUES(1,'admin','admin')")
+    cur.execute("INSERT INTO users VALUES(2,'user','user')")
+
+    
+    #Create some data for pageinformation
+    cur.execute("CREATE TABLE notes(MessageId INTEGER PRIMARY KEY AUTOINCREMENT, Title TEXT, Body TEXT, UserId INT)")
+    cur.execute("INSERT INTO notes VALUES(1,'First note','This is a secret message that should not be shown to ordinary folks',1)")
+    
+    cur.execute("CREATE TABLE passwordForget(UserName TEXT, ResetToken TEXT)")
+
+    con.commit()
+    #con.close()
