11ba2ef6f1fd0fc5ca1b3b9de17714e50725441b
Bruno Rodrigues <brunocortesrodrigues85@gmail.com>
2022-12-12 11:18:44 +0100

    nodejs cmd fix

--- nodeJs/CMD/app.js
+++ nodeJs/CMD/app.js
@@ -21,4 +21,6 @@ app.use(function (req, res) {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, "0.0.0.0", () => console.log(`Listening on port ${port}...!!!`));
+app.listen(port, "0.0.0.0", () =>
+  console.log(`Listening on port ${port}...!!!`)
+);
